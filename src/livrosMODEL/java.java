b0c04ab24063cfbbdf5a735329db72875fffc28a
nara <nara.sena@aluno.ce.gov.br>
2023-11-29 10:54:19 -0300

    envio do projeto

---
+++ src/livrosMODEL/java.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package livrosMODEL;
+
+/**
+ *
+ * @author labinfo06
+ */
+public class java {
+   private long LIVRO;
+    private String AUTOR;
+    private String DESCRIÇÂO;
+    private double GENERO;
+    private int ANO;
+
+    public long getLIVRO() {
+        return LIVRO;
+    }
+
+    public void setLIVRO(long LIVRO) {
+        this.LIVRO = LIVRO;
+    }
+
+    public String getAUTOR() {
+        return AUTOR;
+    }
+
+    public void setAUTOR(String AUTOR) {
+        this.AUTOR = AUTOR;
+    }
+
+    public String getDESCRIÇÂO() {
+        return DESCRIÇÂO;
+    }
+
+    public void setDESCRIÇÂO(String DESCRIÇÂO) {
+        this.DESCRIÇÂO = DESCRIÇÂO;
+    }
+
+    public double getGENERO() {
+        return GENERO;
+    }
+
+    public void setGENERO(double GENERO) {
+        this.GENERO = GENERO;
+    }
+
+    public int getANO() {
+        return ANO;
+    }
+
+    public void setANO(int ANO) {
+        this.ANO = ANO;
+    }
+    
+    
+    
+}
